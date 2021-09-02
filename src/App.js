81b5f842637cce1ec5f6f2d6e3cfd2cef0054d59
Shahrukh <shahrukh.khan_ccv18@gla.ac.in>
2021-09-02 13:14:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,93 +1,106 @@
-import React, { Component } from "react";
-import Navbar from "./component/navbar";
-import News from "./component/News";
+import "./App.css";
+
+import React, { useState } from "react";
+import NavBar from "./components/navbar";
+import News from "./components/News";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LoadingBar from "react-top-loading-bar";
 
-export class App extends Component {
-  state = {
-    progress: 0,
-  };
-  setProgress = (progress) => {
-    this.setState({
-      progress: progress,
-    });
-  };
-  render() {
-    return (
-      <div>
-        <Router>
-          <Navbar />
-          <LoadingBar color="#f11946" height={2} progress={this.state.progress} />
-          <Switch>
-            <Route exact path="/">
-              <News
-                setProgress={this.setProgress}
-                pageSize={6}
-                key="general"
-                country="in"
-                category="general"
-              />
-            </Route>
-            <Route exact path="/science">
-              <News
-                setProgress={this.setProgress}
-                pageSize={6}
-                key="science"
-                country="in"
-                category="science"
-              />
-            </Route>
-            <Route exact path="/business">
-              <News
-                setProgress={this.setProgress}
-                pageSize={6}
-                key="business"
-                country="in"
-                category="business"
-              />
-            </Route>
-            <Route exact path="/entertainment">
-              <News
-                setProgress={this.setProgress}
-                pageSize={6}
-                key="entertainment"
-                country="in"
-                category="entertainment"
-              />
-            </Route>
-            <Route exact path="/health">
-              <News
-                setProgress={this.setProgress}
-                pageSize={6}
-                key="health"
-                country="in"
-                category="health"
-              />
-            </Route>
-            <Route exact path="/technology">
-              <News
-                setProgress={this.setProgress}
-                pageSize={6}
-                key="science"
-                country="in"
-                category="technology"
-              />
-            </Route>
-            <Route exact path="/sport">
-              <News
-                setProgress={this.setProgress}
-                pageSize={6}
-                key="sport"
-                country="in"
-                category="sports"
-              />
-            </Route>
-          </Switch>
-        </Router>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const pageSize = 5;
+  const apiKey = "b586564b47cd4760a7f2ff80875fd523";
+  const [progress, setProgress] = useState(0);
+
+  return (
+    <div>
+      <Router>
+        <NavBar />
+        <LoadingBar height={3} color="#f11946" progress={progress} />
+        <Switch>
+          <Route exact path="/">
+            <News
+              setProgress={setProgress}
+              apiKey={apiKey}
+              key="general"
+              pageSize={pageSize}
+              country="in"
+              category="general"
+            />
+          </Route>
+          <Route exact path="/business">
+            <News
+              setProgress={setProgress}
+              apiKey={apiKey}
+              key="business"
+              pageSize={pageSize}
+              country="in"
+              category="business"
+            />
+          </Route>
+          <Route exact path="/entertainment">
+            <News
+              setProgress={setProgress}
+              apiKey={apiKey}
+              key="entertainment"
+              pageSize={pageSize}
+              country="in"
+              category="entertainment"
+            />
+          </Route>
+          <Route exact path="/general">
+            <News
+              setProgress={setProgress}
+              apiKey={apiKey}
+              key="general"
+              pageSize={pageSize}
+              country="in"
+              category="general"
+            />
+          </Route>
+          <Route exact path="/health">
+            <News
+              setProgress={setProgress}
+              apiKey={apiKey}
+              key="health"
+              pageSize={pageSize}
+              country="in"
+              category="health"
+            />
+          </Route>
+          <Route exact path="/science">
+            <News
+              setProgress={setProgress}
+              apiKey={apiKey}
+              key="science"
+              pageSize={pageSize}
+              country="in"
+              category="science"
+            />
+          </Route>
+          <Route exact path="/sports">
+            <News
+              setProgress={setProgress}
+              apiKey={apiKey}
+              key="sports"
+              pageSize={pageSize}
+              country="in"
+              category="sports"
+            />
+          </Route>
+          <Route exact path="/technology">
+            <News
+              setProgress={setProgress}
+              apiKey={apiKey}
+              key="technology"
+              pageSize={pageSize}
+              country="in"
+              category="technology"
+            />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,17 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class Spnner extends Component {
-  render() {
+const Spinner = () => {
     return (
-      <div>
+      <>
         <div className="d-flex my-3 justify-content-center">
           <div className="spinner-border" role="status">
             <span className="visually-hidden">Loading...</span>
           </div>
         </div>
-      </div>
+      </>
     );
   }
-}
 
-export default Spnner;
+
+export default Spinner;
